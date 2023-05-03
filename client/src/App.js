3941815ee34acae3307912e2638ca7eb555631a8
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-05-02 21:43:58 -0500

    Set up client/server organization

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body></body>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
