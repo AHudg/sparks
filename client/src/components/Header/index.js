eee51826bde0291cde35ede58d58217577601b5d
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-05-08 21:35:19 -0500

    Fixed issue where initialization of app was always as mobile

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from "react";
 import homeLogo from "../../homeLogo.png";
 
 export default function Header() {
-  const [mobile, setMobile] = useState(true);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const breakpoint = 768;
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 768) {
-        setMobile(true);
-      } else {
-        setMobile(false);
-      }
+      setScreenWidth(window.innerWidth);
     };
     window.addEventListener("resize", handleResize);
 
@@ -21,18 +18,20 @@ export default function Header() {
 
   return (
     <header className="">
-      <a href="/">
-        <img src={homeLogo} className="homeLogo"></img>
-      </a>
-      {mobile === true ? (
-        <div>
-          <span className="topHamburger"></span>
-          <span className="middleHamburger"></span>
-          <span className="bottomHamburger"></span>
-        </div>
-      ) : (
-        <div className="navigation">hello</div>
-      )}
+      <div className="row justify-content-between">
+        <a href="/" className="col-2">
+          <img src={homeLogo} className="homeLogo"></img>
+        </a>
+        {screenWidth < breakpoint ? (
+          <div className="col-2 row justify-content-end">
+            <span className="topHamburger"></span>
+            <span className="middleHamburger"></span>
+            <span className="bottomHamburger"></span>
+          </div>
+        ) : (
+          <div className="navigation">hello</div>
+        )}
+      </div>
     </header>
   );
 }
