97a8c95901628c934c48aa419180441b4a9e13a7
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-05-16 21:27:32 -0500

    Created hamburger and added the menu to open- need closure

--- client/src/components/Hamburger/index.js
+++ client/src/components/Hamburger/index.js
@@ -2,12 +2,25 @@ import React from "react";
 
 export default function Hamburger() {
   return (
-    <nav className="hamburger">
-      <ul className="">
-        <li>
-          <a></a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <div className="menu">
+        <nav>
+          <ul className="row justify-content-center">
+            <li className="col-12">
+              <a href="/blog">Blog</a>
+            </li>
+            <li className="col-12">
+              <a href="/about">About</a>
+            </li>
+            <li className="col-12">
+              <a>Store</a>
+            </li>
+            <li className="col-12">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,6 +5,8 @@ export default function Header() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const breakpoint = 768;
 
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
@@ -16,15 +18,18 @@ export default function Header() {
     };
   }, []);
 
+  const handleMobileNav = () => {
+    setOpen(!open);
+  };
+
   return (
     <header className="">
       <div className="row justify-content-between">
         <a href="/" className="col-2">
           <img src={homeLogo} className="homeLogo"></img>
         </a>
-        <h1 classname="col-3">Kindling by J</h1>
         {screenWidth < breakpoint ? (
-          <div className="col-7 hamburger">
+          <div className="col-10 hamburger" onClick={handleMobileNav}>
             <span className="topHamburger"></span>
             <span className="middleHamburger"></span>
             <span className="bottomHamburger"></span>
@@ -32,22 +37,45 @@ export default function Header() {
         ) : (
           <nav className="col-8">
             <ul className="row">
-              <li className="col-3 nav">
+              <li className="col-3">
                 <a href="/blog">Blog</a>
               </li>
-              <li className="col-3 nav">
+              <li className="col-3">
                 <a href="/about">About</a>
               </li>
-              <li className="col-3 nav">
+              <li className="col-3">
                 <a>Store</a>
               </li>
-              <li className="col-3 nav">
+              <li className="col-3">
                 <a href="#contact">Contact</a>
               </li>
             </ul>
           </nav>
         )}
       </div>
+
+      {open === true ? (
+        <div className="mobileNav">
+          <nav>
+            <ul className="row justify-content-center">
+              <li className="col-12">
+                <a href="/blog">Blog</a>
+              </li>
+              <li className="col-12">
+                <a href="/about">About</a>
+              </li>
+              <li className="col-12">
+                <a>Store</a>
+              </li>
+              <li className="col-12">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </header>
   );
 }
