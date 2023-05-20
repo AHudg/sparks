e44be77c51df371dc44f4a3ff6f0a777f732cd57
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-05-19 21:34:34 -0500

    Finished functioning hamburger animations

--- client/src/components/Hamburger/index.js
+++ client/src/components/Hamburger/index.js
@@ -1,81 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { gsap } from "gsap";
-import { Timeline } from "gsap/gsap-core";
 
 export default function Hamburger() {
   const [open, setOpen] = useState(false);
 
   const handleMenu = () => {
     setOpen(!open);
-  };
 
-  // {open === true ? (
-  //   <div className="menu">
-  //     <nav>
-  //       <ul className="row justify-content-center">
-  //         <li className="col-12">
-  //           <a href="/blog">Blog</a>
-  //         </li>
-  //         <li className="col-12">
-  //           <a href="/about">About</a>
-  //         </li>
-  //         <li className="col-12">
-  //           <a>Store</a>
-  //         </li>
-  //         <li className="col-12">
-  //           <a href="#contact">Contact</a>
-  //         </li>
-  //       </ul>
-  //     </nav>
-  //   </div>
-  // ) : (
-  //   <p>closed</p>
-  // )}
+    // this is counterintuitive- I change the status of open first
+    // thus I use false for when the menu really is open
+    if (open === false) {
+      const openTl = gsap.timeline();
+      openTl.to(".ribbonOne", { y: window.innerHeight });
+      openTl.to(".ribbonTwo", { y: window.innerHeight });
+      openTl.to(".ribbonThree", { y: window.innerHeight });
+      openTl.to(".hamMiddle", { backgroundColor: "rgba( 192, 58, 0, 0" });
+      openTl.to(".hamTop", { rotate: 45, y: 10 });
+      openTl.to(".hamBottom", { rotate: -45, y: -10 }, "=-.5");
+    } else {
+      const closeTl = gsap.timeline();
+      closeTl.to(".hamTop", { rotate: 0, y: 0 });
+      closeTl.to(".hamBottom", { rotate: 0, y: 0 }, -0.001);
+      closeTl.to(".hamMiddle", { backgroundColor: "rgba( 192, 58, 0, 1" });
+      closeTl.to(".ribbonOne", { y: -window.innerHeight }, "=-.5");
+      closeTl.to(".ribbonTwo", { y: -window.innerHeight });
+      closeTl.to(".ribbonThree", { y: -window.innerHeight });
+    }
+  };
 
   return (
     <div>
-      <svg width="300" height="300">
-        <svg
-          version="1.1"
-          baseProfile="tiny"
-          id="Layer_1"
-          x="0px"
-          y="0px"
-          width="570px"
-          height="150px"
-          viewBox="0 0 570 150"
-        ></svg>
+      <div className="hamBorder" onClick={handleMenu}>
+        <span className="hamTop"></span>
+        <span className="hamMiddle"></span>
+        <span className="hamBottom"></span>
+      </div>
+      <div className="row">
+        <span className="ribbonOne"></span>
+        <span className="ribbonTwo"></span>
+        <span className="ribbonThree"></span>
+      </div>
+      {open === true ? (
+        <div className="menu">
+          <nav>
+            <ul className="row justify-content-center align-items-center">
+              <li className="col-12">
+                <a href="/blog">Blog</a>
+              </li>
 
-        {open === true ? (
-          <circle
-            cx="11"
-            cy="11"
-            r="9"
-            stroke="#ff840080"
-            strokeWidth="3"
-            fill="#ff840080"
-            className="rectangle"
-            onClick={handleMenu}
-          ></circle>
-        ) : (
-          <rect
-            className="rectangle"
-            x="0"
-            y="0"
-            width="100"
-            height="100"
-            stroke="#ff840080"
-            strokeMiterlimit="10"
-            strokeWidth="5"
-            strokeOpacity={0}
-            fillOpacity={0.5}
-            id="rectang"
-            fill="#ff840080"
-            onClick={handleMenu}
-            onMouseEnter={() => gsap.to("rect", { y: 100, strokeOpacity: 1 })}
-          ></rect>
-        )}
-      </svg>
+              <li className="col-12">
+                <a href="/about">About</a>
+              </li>
+
+              <li className="col-12">
+                <a>Store</a>
+              </li>
+
+              <li className="col-12">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      ) : (
+        <span></span>
+      )}
     </div>
   );
 }
