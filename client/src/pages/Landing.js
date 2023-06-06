0b0e2a8adbed2449dbb8fd46ef34deb3fd8abea4
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-06-05 20:40:18 -0500

    Styled landing, added about & shop section to landing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -7,17 +7,18 @@ function Landing() {
   return (
     <main>
       <section className="sectBlog row">
-        <div className="col-12 slogan">
+        <div className="col-12 ornamentBlog">
           <p className="">IGNITING</p>
-          <p className="">INSPIRATIONS</p>
+          <p className="">INSPIRATION</p>
         </div>
 
-        <div className="col-7 row">
+        <div className="col-7 row mx-4">
           <p className="col-12 descBlog">
             Built to uplift. We illuminate your interest and dreams so that you
-            can start living your own life.
+            can start living your own life. Here is your spark, now ignite your
+            inspiration.
           </p>
-          <a className="col-8 mx-4 mb-5 text-center buttonBlog" href="/blog">
+          <a className="col-8 mx-3 mb-5 text-center buttonBlog" href="/blog">
             Visit the Blog!
           </a>
         </div>
@@ -26,18 +27,35 @@ function Landing() {
       </section>
 
       <section className="sectAbout">
-        <div className="slogan vertical">
-          <p className="">CONVERSATION</p>
-          <p className="">STIMULATING</p>
+        <div className="ornamentAbout">
+          <p className="">OUR</p>
+          <p className="">STORY</p>
         </div>
 
-        <div className="col-7 row">
-          <p className="col-12 descBlog">
-            Built to uplift. We illuminate your interest and dreams so that you
-            can start living your own life.
+        <div className="col-7 row mx-4">
+          <p className="col-12 descAbout">
+            Care to learn how we started? Want to meet the team? Enjoy
+            discovering who we are and what environment we strive to foster.
           </p>
-          <a className="col-8 mx-4 mb-5 text-center buttonBlog" href="/blog">
-            Visit the Blog!
+          <a className="col-8 mx-4 mb-5 text-center buttonAbout" href="/blog">
+            Read Our Story!
+          </a>
+        </div>
+      </section>
+
+      <section className="sectShop">
+        <div className="ornamentShop">
+          <p className="">SPREAD</p>
+          <p className="">THE WORD</p>
+        </div>
+
+        <div className="col-7 row mx-4">
+          <p className="col-12 descShop">
+            Care to learn how we started? Want to meet the team? Enjoy
+            discovering who we are and what environment we strive to foster.
+          </p>
+          <a className="col-8 mx-4 mb-5 text-center buttonShop" href="/blog">
+            Read Our Story!
           </a>
         </div>
       </section>
