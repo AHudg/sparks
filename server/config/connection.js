7d7c779a59e82ab1f01aa5f4dbe60c31b9eb466f
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-10-12 20:43:47 -0500

    Trying everything

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
-console.log(process.env.REACT_APP_URI);
-console.log(process.env.REACT_APP_PORT);
 
 // tells mongoose which database we want to connect with
 // 127.0..0.1:27017 is used to connect w/ the localhost
 mongoose.connect(
-  process.env.REACT_APP_URI || "mongodb://127.0.0.1:27017/sparks",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sparks",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
